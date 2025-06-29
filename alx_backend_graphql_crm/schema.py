47abe0d707c2e5f5430b0759eddfa7f9136430a6
reuben-idan <nobeadonis2@gmail.com>
2025-06-29 18:56:23 +0000

    Update code snippet

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -4,7 +4,7 @@ from crm.schema import Query as CRMQuery
 from crm.schema import Mutation as CRMMutation
 
 class Query(CRMQuery, graphene.ObjectType):
-    pass
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path
 from graphene_django.views import GraphQLView
-from crm.schema import schema
 from django.views.decorators.csrf import csrf_exempt
+from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crm/', include('crm.urls')),
-    re_path(r"graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)), name='graphql'),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
